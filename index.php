0f7d6f7d67114edffe6ab6b59b9dbce88b5c8312
PIeter <pieter_ophalvens@hotmail.com>
2024-01-09 14:19:21 +0100

    ex1 no classess

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+
+//without classess
+$banana = 1;
+$apple = 1.5;
+$wine = 10;
+echo $banana * 6 + $apple * 3 + $wine * 2 . 'price total';
+echo $banana * 6 * 0.06 + $apple * 3 * 0.06 + $wine * 2 * 0.21 . 'tax';
